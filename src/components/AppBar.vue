ce1e182b6cb0b927535ccfd430160cfc23db435f
Joshua Ferris <jajjferris@gmail.com>
2022-04-22 23:36:11 -0400

    :lipstick: updated emerald color of logo

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -7,7 +7,7 @@ const auth = useAuthStore();
 <template>
   <nav class="px-4 py-2 flex justify-between items-center mb-6">
     <div>
-      <h1 class="text-xl sm:text-3xl text-gray-800">Hear<span class="text-emerald-400">Ye</span>Hear<span class="text-emerald-400">Ye</span></h1>
+      <h1 class="text-xl sm:text-3xl text-gray-800">Hear<span class="text-emerald-500">Ye</span>Hear<span class="text-emerald-500">Ye</span></h1>
     </div>
     <div v-if="auth.user">
       <button @click="() => auth.logout()" class="focus:outline-none text-red-600 text-sm py-2 px-5 rounded-full border border-red-600 hover:bg-red-50">Log Out</button>
